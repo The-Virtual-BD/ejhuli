d0cc8da5ef053d6c97e660899ea69ce8a3a594f1
Hussains3 <shussain8978@gmail.com>
2023-03-23 17:51:30 +0600

    Chekou Page 1.01

--- database/migrations/2020_06_12_124520_create_orders_table.php
+++ database/migrations/2020_06_12_124520_create_orders_table.php
@@ -23,9 +23,11 @@ class CreateOrdersTable extends Migration
             $table->unsignedTinyInteger('payment_method')->comment('1=>Wallet,2=>COD');
             $table->unsignedDecimal('payment_amount',8,2);
             $table->unsignedDecimal('delivery_charge',8,2)->nullable();
+            $table->unsignedInteger('delivery_charge_status')->default(1)->comment('1=>Unpaid,2=>Paid');
             $table->string('additional_info')->nullable();
             $table->string('discounts')->nullable();
             $table->unsignedInteger('status')->default(1)->comment('1=>Pending,2=>Confirmed,3=>Delivered,4=>Canceled');
+            $table->unsignedInteger('type')->default(1)->comment('1=>General,2=>Pree');
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
